ce80e37e36a27a453a0f6576bdb17c7ce2e7691b
tomoshino <zm18214@josai.ac.jp>
2019-11-12 03:30:43 +0000

    11/12

--- ex6-5.js
+++ ex6-5.js
@@ -1,11 +1,11 @@
-var who = 'world';
-function sayhello(){
- alert( who +'が来た'+'!');
-}
-function taro(){
- who='太郎';
-}
-function hanako(){
- who ='花子';
-}
 
+var who65='world';
+function sayhello65(){
+ alert('Hello,' + who65 +'!');
+}
+function taro65(){
+ who65 = '太郎';
+}
+function hanako65(){
+ who65 = '花子';
+}
